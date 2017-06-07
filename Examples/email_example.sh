997a6c1f3912ae697e522d3354facca71492d74b
victoriachistolini <vachisto@colby.edu>
2017-06-07 15:18:59 -0400

    Update email_example.sh

--- Examples/email_example.sh
+++ Examples/email_example.sh
@@ -1,1 +1,28 @@
 
+#!/bin/bash                                                                     
+
+## set name of PBS job                                                           
+#PBS -N basic_submission_script                                                             
+
+## send the environment variables with job 
+#PBS -V
+
+## set the queue                                                                          
+#PBS -q route                                                                   
+
+## give job 10 minutes                        
+#PBS -l walltime=00:10:00 
+
+## use one compute node and one cpu (this will default to use 2gb of memory)                                                      
+#PBS -l select=1:ncpus=1                                                        
+                                                              
+## output files placed in output directory in the user vcc’s home directory 
+## **** to use this script change vcc to your Bigelow username **** 
+#PBS -e /home/vcc/output                                                           
+#PBS -o /home/vcc/output                                                          
+
+
+## jobs to submit                                                               
+echo start
+/bin/sleep 10
+echo finished
