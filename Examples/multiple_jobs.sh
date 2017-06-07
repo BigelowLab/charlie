6d550f1036704a3ab32d39bd4972c0637621b9bc
victoriachistolini <vachisto@colby.edu>
2017-06-07 08:47:56 -0400

    Update and rename multiple_jobs to multiple_jobs.sh

---
+++ Examples/multiple_jobs.sh
@@ -0,0 +1,12 @@
+
+#!/bin/bash  
+
+## set name of script                                                           
+#PBS -N multiple_jobs_example_script                                                             
+
+## load environment variables 
+#PBS -V                    
+
+## set the queue for the jobs
+#PBS -q route 
+
